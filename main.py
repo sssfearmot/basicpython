0aff8b2ecfa509b89870916070d068ed57fcf2f6
sssfearmt <story_mania@naver.com>
2021-12-02 19:22:33 +0900

    add function about save

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-inventory = {}
+import pickle
 
 def add_item(item, amount, t_inventory):
     if check_item(item, t_inventory):
@@ -48,10 +48,28 @@ def use_item():
             pass
         else:
             print("잘못된 번호를 입력하셨습니다")
+'''
+try:
+    load_file = open("game_save.p", "rb")
+    character = pickle.load(load_file)
+    load_file.close()
+    print("저장된 파일을 읽어왔습니다")
+except:
+    print("읽어올 파일이 없습니다")
+    character = {}
+'''
 
+import os
 
-
-character = {}
+if os.path.isfile("game_save.p"):
+    load_file = open("game_save.p", "rb")
+    character = pickle.load(load_file)
+    load_file.close()
+    print("저장된 파일을 읽어왔습니다")
+else:
+    print("읽어올 파일이 없습니다")
+    character = {}
+    
 select_character = None
 def new_character(name, t_character):
     if check_character (name, t_character):
@@ -64,7 +82,7 @@ def check_character (name, t_character):
     return name in t_character
 
 def print_characterMenu():
-    print("0. 끝내기")
+    print("0. 저장하고 끝내기")
     print("1. 캐릭터 추가")
     print("2. 캐릭터 이름출력")
     print("3. 캐릭터 선택")
@@ -74,6 +92,10 @@ while True:
     print_characterMenu()
     option = int(input("메뉴를 선택해주세요"))
     if option == 0:
+        save_file = open("game_save.p","wb")
+        pickle.dump(character, save_file)
+        save_file.close()
+        print("게임 내용이 저장되었습니다.")
         print("종료되었습니다")
         break
     elif option == 1:
